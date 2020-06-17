9cb964d0b17c965ef3ff21c4616505d5630311fb
Nxalenye Sinyanya <nsinyany@student.wethinkcode.co.za>
2020-06-17 13:46:57 +0200

    ls -l command listing files and directories.

---
+++ ex14/skip.sh
@@ -0,0 +1,7 @@
+drwxr-xr-x  3 nsinyany  student  96 Jun 17 13:17 ex00
+drwxr-xr-x  3 nsinyany  student  96 Jun 17 13:17 ex02
+drwxr-xr-x  3 nsinyany  student  96 Jun 17 13:17 ex04
+drwxr-xr-x  3 nsinyany  student  96 Jun 17 13:17 ex06
+drwxr-xr-x  3 nsinyany  student  96 Jun 17 13:17 ex08
+drwxr-xr-x  3 nsinyany  student  96 Jun 17 13:21 ex10
+drwxr-xr-x  3 nsinyany  student  96 Jun 17 13:35 ex12
